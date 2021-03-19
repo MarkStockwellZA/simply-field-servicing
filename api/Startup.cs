001d9f938f02c8038438fda6a5752725c7046767
Mark Stockwell <markstockwell2007@gmail.com>
2021-03-19 16:49:53 +0200

    Set the swagger title

--- api/Startup.cs
+++ api/Startup.cs
@@ -33,7 +33,7 @@ namespace SimplyFieldServicing
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "simply_field_servicing", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Simply Field Servicing", Version = "v1" });
             });
         }
 
